5dd490b95b0ba4c6ce488d8bc5eb1a4de86fee44
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-18 19:23:44 +0300

    make the products

---
+++ components/data/products.js
@@ -0,0 +1,17 @@
+const product = [{
+    id: 1,
+    name: 'Syltherine',
+    category: "Stylish cafe chair",
+    price: 2500000,
+    oldPrice: 3500000,
+    image: "/assets/images/productOne.svg",
+    discount: "-30%"
+},
+{
+    id: 2,
+    name: "Leviosa",
+    category: "Stylish cafe chair",
+    price: 2500000,
+    image: "/assets/images/productTwo.svg"
+}
+]
