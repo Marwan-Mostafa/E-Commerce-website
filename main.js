07a01659bdd51fa14b3058e0491227663b6753de
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-21 21:47:23 +0300

    create show more and make update of UI

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { addToCart } from "./state/cart.js";
 import { toggleWishlist } from "./state/wishlist.js";
 
 const app = document.getElementById("app");
+let visibleProducts = 4
 
 // Render UI
 function render() {
@@ -15,14 +16,38 @@ function render() {
           Our Products
         </h2>
 
-        ${ProductGrid(products)}
+        ${ProductGrid(products.slice(0, visibleProducts))}
+
+        <div class="flex justify-center mt-10">
+          
+        <button id="show-more-btn" class="border border-yellow-700 text-yellow-700 px-10 py-3 
+          font-semibold hover:bg-yellow-700 hover:text-white transition duration-300 cursor-pointer">
+            Show more    
+        
+        </button>
+        
+        </div>
 
       </div>
     </section>
   `;
 }
 
+function setupEvent(){
+  const showMoreBtn = document.getElementById("show-more-btn")
+  
+  if (!showMoreBtn) return
+
+  showMoreBtn.addEventListener("click", ()=>{
+    visibleProducts += 4
+
+    render()
+    setupEvent()
+  })
+}
+
 render();
+setupEvent();
 
 window.addToCartHandler = (id) => {
     const product = products.find(p => p.id === id);
