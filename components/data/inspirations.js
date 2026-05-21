e4942ff0be18fd1234883b3b09d25dbef2143104
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-21 22:34:35 +0300

    make the inspiration data of product

---
+++ components/data/inspirations.js
@@ -0,0 +1,14 @@
+const inspiration = [
+    {
+    id: 1,
+    title: "Inner Peace",
+    category: "Bed Room",
+    image: "../assets/room1.png"
+  },
+  {
+    id: 2,
+    title: "Minimal Space",
+    category: "Living Room",
+    image: "../assets/room2.png"
+  }
+]
