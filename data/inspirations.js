958c785265d7ea34cd5df636b7bfa7d11cbef29a
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-22 18:32:38 +0300

    make the inspiration file of the rooms

---
+++ data/inspirations.js
@@ -0,0 +1,20 @@
+const inspirations = [
+    {
+        id: 1,
+        room: "Bed Room",
+        name: "Inner Peace",
+        images: ["assets/images/room1a.jpg", "assets/images/room1b.jpg"],
+    },
+    {
+        id: 2,
+        room: "Living Room",
+        name: "Natural Wood",
+        images: ["assets/images/room2a.jpg", "assets/images/room2b.jpg"],
+    },
+    {
+        id: 3,
+        room: "Dining Room",
+        name: "Calm Space",
+        images: ["assets/images/room3a.jpg", "assets/images/room3b.jpg"],
+    },
+]
