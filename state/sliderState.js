ca0885aee93cbc4e368b93864df990c6b75a0624
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-25 11:25:45 +0300

    fix and make the all functions of slider

--- state/sliderState.js
+++ state/sliderState.js
@@ -1,15 +1,17 @@
-export function createSlider(trackEl, dots, total) {
-    let current = 0
+let state = {
+    currentIndex : 0,
+    total: 0
+}
 
-    function goTo(index) {
-        current = (index + total) % total
-        trackEl.style.transform = `translateX(-${current * 100}%)`
-        dots.forEach((dot, i) => {
-            dot.classList.toggle("active", i === current)
-        })
-    }
-    return {
-        goTo,
-        getCurrent: () => current
-    }
+function getSliderState(){
+    return {...state}
+}
+
+function setSliderIndex(newIndex){
+    state.currentIndex = newIndex
+}
+
+function initSliderState(total){
+    state.total = total
+    state.currentIndex = 0
 }
