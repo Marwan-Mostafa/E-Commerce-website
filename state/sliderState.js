c5e124e42e2ce4710e6204b4d12a18537da1fe64
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-25 11:31:51 +0300

    Add comments and export functions from slider.js

--- state/sliderState.js
+++ state/sliderState.js
@@ -1,17 +1,26 @@
+// This is data of current card
+
 let state = {
     currentIndex : 0,
     total: 0
 }
 
-function getSliderState(){
+// Function for Read Data and Copy new Object with spread Operator
+
+export function getSliderState(){
     return {...state}
 }
 
-function setSliderIndex(newIndex){
+//  Function for Edit data
+
+
+export function setSliderIndex(newIndex){
     state.currentIndex = newIndex
 }
 
-function initSliderState(total){
+// Initialization function for cards
+
+export function initSliderState(total){
     state.total = total
     state.currentIndex = 0
 }
