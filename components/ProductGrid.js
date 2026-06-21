3c3af7caa139c1655a1d3c4b2018411f9adbfc70
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-06-21 17:03:42 +0300

    feat(grid): implement the dynamic logic

---
+++ components/ProductGrid.js
@@ -0,0 +1,12 @@
+const gridView = document.getElementById("gridView")
+const listView = document.getElementById("listView")
+
+gridView.addEventListener("click", () => {
+    productsContainer.classList.remove("list-layout");
+    productsContainer.classList.add("grid-layout");
+});
+
+listView.addEventListener("click", () => {
+    productsContainer.classList.remove("grid-layout");
+    productsContainer.classList.add("list-layout");
+});
