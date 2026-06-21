08c269dff67c64cad50914e6864e8c9d3bcf4873
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-06-21 18:15:14 +0300

    Add more products

--- data/products.js
+++ data/products.js
@@ -60,4 +60,190 @@ export const products = [{
     price: 500000,
     image: "/assets/images/productEight.svg"
 },
+{
+    id: 9,
+    name: 'Syltherine',
+    category: "Stylish cafe chair",
+    price: 2500000,
+    oldPrice: 3500000,
+    image: "/assets/images/productOne.svg",
+    discount: "-30%",
+},
+{
+    id: 10,
+    name: "Lolito",
+    category: "Stylish cafe chair",
+    price: 2500000,
+    image: "/assets/images/productTwo.svg",
+},
+{
+    id: 11,
+    name: "Leviosa",
+    category: "Luxury big sofa",
+    price:  7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productThree.svg",
+    discount: "-50%",
+},
+{
+    id: 12,
+    name: "Respira",
+    category: "Outdoor bar table and stool",
+    price: 500000,
+    image: "/assets/images/productFour.svg"
+},
+{
+    id: 13,
+    name: "Grifo",
+    category: "Night lamp",
+    price:  1500000,
+    image: "/assets/images/productFive.svg"
+},
+{
+    id: 14,
+    name: "Muggo",
+    category: "Small mug",
+    price: 150000,
+    image: "/assets/images/productSix.svg"
+},
+{
+    id: 14,
+    name: "Pingky",
+    category: "Cute bed set",
+    price: 7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productSeven.svg",
+    discount: "-50%",
+},
+{
+    id: 16,
+    name: "Potty",
+    category: "Minimalist flower pot",
+    price: 500000,
+    image: "/assets/images/productEight.svg"
+},
+{
+    id: 17,
+    name: 'Syltherine',
+    category: "Stylish cafe chair",
+    price: 2500000,
+    oldPrice: 3500000,
+    image: "/assets/images/productOne.svg",
+    discount: "-30%",
+},
+{
+    id: 18,
+    name: "Lolito",
+    category: "Stylish cafe chair",
+    price: 2500000,
+    image: "/assets/images/productTwo.svg",
+},
+{
+    id: 19,
+    name: "Leviosa",
+    category: "Luxury big sofa",
+    price:  7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productThree.svg",
+    discount: "-50%",
+},
+{
+    id: 20,
+    name: "Respira",
+    category: "Outdoor bar table and stool",
+    price: 500000,
+    image: "/assets/images/productFour.svg"
+},
+{
+    id: 21,
+    name: "Grifo",
+    category: "Night lamp",
+    price:  1500000,
+    image: "/assets/images/productFive.svg"
+},
+{
+    id: 22,
+    name: "Muggo",
+    category: "Small mug",
+    price: 150000,
+    image: "/assets/images/productSix.svg"
+},
+{
+    id: 23,
+    name: "Pingky",
+    category: "Cute bed set",
+    price: 7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productSeven.svg",
+    discount: "-50%",
+},
+{
+    id: 24,
+    name: "Potty",
+    category: "Minimalist flower pot",
+    price: 500000,
+    image: "/assets/images/productEight.svg"
+},
+{
+    id: 25,
+    name: 'Syltherine',
+    category: "Stylish cafe chair",
+    price: 2500000,
+    oldPrice: 3500000,
+    image: "/assets/images/productOne.svg",
+    discount: "-30%",
+},
+{
+    id: 26,
+    name: "Lolito",
+    category: "Stylish cafe chair",
+    price: 2500000,
+    image: "/assets/images/productTwo.svg",
+},
+{
+    id: 27,
+    name: "Leviosa",
+    category: "Luxury big sofa",
+    price:  7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productThree.svg",
+    discount: "-50%",
+},
+{
+    id: 28,
+    name: "Respira",
+    category: "Outdoor bar table and stool",
+    price: 500000,
+    image: "/assets/images/productFour.svg"
+},
+{
+    id: 29,
+    name: "Grifo",
+    category: "Night lamp",
+    price:  1500000,
+    image: "/assets/images/productFive.svg"
+},
+{
+    id: 30,
+    name: "Muggo",
+    category: "Small mug",
+    price: 150000,
+    image: "/assets/images/productSix.svg"
+},
+{
+    id: 31,
+    name: "Pingky",
+    category: "Cute bed set",
+    price: 7000000,
+    oldPrice: 14000000,
+    image: "/assets/images/productSeven.svg",
+    discount: "-50%",
+},
+{
+    id: 32,
+    name: "Potty",
+    category: "Minimalist flower pot",
+    price: 500000,
+    image: "/assets/images/productEight.svg"
+},
 ]
