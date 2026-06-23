4e66cb5c05935a738d17522cf8ac0036c579b5c9
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-06-24 02:14:18 +0300

    feat(shop): Implement all logics of the shop page

--- pages/shop.js
+++ pages/shop.js
@@ -1,43 +1,45 @@
-import {products} from "../data/products.js"
-import {paginate, getTotalPages} from "../utils/paginate.js"
+import { products } from "../data/products.js"
+import { paginate, getTotalPages } from "../utils/paginate.js"
+import { state } from "../state/shopState.js"
+import { sortProducts } from "../utils/sortProducts.js";
+import { setupFilters } from "../components/ FilterBar.js";
 import { ProductGrid } from "../components/ProductGrid.js"
 import { Pagination } from "../components/ Pagination.js"
 
 
-let currentPage = 1
-const perPage = 8
-
 const shopContainer = document.getElementById("shopContainer")
 
-function renderShop(){
-    const paginateProducts = paginate(products, currentPage, perPage)
-    const totalPages = getTotalPages(products, perPage)
+function renderShop() {
+    const sortedProducts = sortProducts(products, state.sortBy);
+
+    const paginatedProducts = paginate(sortedProducts, state.currentPage, state.perPage)
+    const totalPages = getTotalPages(sortedProducts, state.perPage)
 
     shopContainer.innerHTML = `
-    ${ProductGrid(paginateProducts)}
-    ${Pagination(totalPages, currentPage)}
+    ${ProductGrid(paginatedProducts, state.viewMode)}
+    ${Pagination(totalPages, state.currentPage)}
     `
 
     attachPaginationEvents(totalPages);
 }
 
-function attachPaginationEvents(totalPages){
+function attachPaginationEvents(totalPages) {
     const pageButton = document.querySelectorAll("[data-page]")
     const nextButton = document.querySelector("[data-next]")
 
-    pageButton.forEach((button) =>{
-        button.addEventListener("click", ()=>{
-            currentPage = Number(button.dataset.page)
+    pageButton.forEach((button) => {
+        button.addEventListener("click", () => {
+            state.currentPage = Number(button.dataset.page)
             renderShop()
         })
     })
 
-    nextButton.addEventListener("click", ()=>{
-        if(currentPage < totalPages){
-            currentPage++
+    nextButton.addEventListener("click", () => {
+        if (state.currentPage < totalPages) {
+            state.currentPage++
             renderShop()
         }
     })
 }
-
+setupFilters(renderShop, products)
 renderShop()
