e480c2ba2ab6d3cbfa22c994d6deadef606de502
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-06-23 13:01:49 +0300

    feat(pagination):Implement the logic of the Pagination

---
+++ utils/paginate.js
@@ -0,0 +1,10 @@
+export function paginate(items, page, perPage) {
+  const start = (page - 1) * perPage;
+  const end = start + perPage;
+
+  return items.slice(start, end);
+}
+
+export function hasMore(items, perPage) {
+  return Math.ceil(items.length / perPage);
+}
