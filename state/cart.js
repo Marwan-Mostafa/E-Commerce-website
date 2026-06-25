6b0bc7db04ccd2e0221751792b3a0cff94c895b6
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-06-25 19:07:05 +0300

    feat(): Implement the dynamic logic of the cart

--- state/cart.js
+++ state/cart.js
@@ -1,10 +1,48 @@
-let cart = []
+// let cart = []
 
-export function addToCart(product){
-    cart.push(product)
-    console.log("cart: ", cart)
+// export function addToCart(product){
+//     cart.push(product)
+//     console.log("cart: ", cart)
+// }
+
+// export function getCart(){
+//     return cart
+// }
+
+const CART_KEY = "cart"
+
+let cart = JSON.parse(localStorage.getItem(CART_KEY)) || []
+
+function saveCart() {
+    localStorage.setItem(CART_KEY, JSON.stringify(cart))
 }
 
-export function getCart(){
-    return cart
+export function addToCart(product) {
+    const existingItem = cart.find((item) => {
+        item.id === product.id && item.size === product.size && item.color === product.color
+    })
+
+    if (existingItem) {
+        existingItem.quantity += product.quantity
+    } else {
+        cart.push(print)
+    }
+
+    saveCart()
+    console.log("Cart:", cart)
+}
+
+export function removeFromCart(productId) {
+
+    cart = cart.filter((item) => item.id !== productId);
+    saveCart();
+}
+
+export function getCart() {
+    return cart;
+}
+
+export function clearCart() {
+    cart = [];
+    saveCart();
 }
