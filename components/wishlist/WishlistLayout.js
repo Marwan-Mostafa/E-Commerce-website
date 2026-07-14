953569f2b26e3a9fe6c1f4a6b6938a94b82ae7ca
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-07-14 09:05:10 +0300

    update and refactor the wishlist layout

--- components/wishlist/WishlistLayout.js
+++ components/wishlist/WishlistLayout.js
@@ -1,6 +1,5 @@
 import { WishlistHeader } from "./WishlistHeader.js";
 import { WishlistEmpty } from "./WishlistEmpty.js";
-
 import { renderProductGrid } from "../ProductGrid.js";
 
 const WRAPPER_CLASS = `
@@ -9,34 +8,20 @@ flex-col
 gap-14
 `;
 
-export function WishlistLayout({
 
-    products = [],
+function trimClassList(classList) {
+    return classList.split(/\s+/).filter(Boolean).join(" ");
+}
 
-} = {}) {
 
-    const hasProducts = products.length > 0;
+export function WishlistLayout({ products = [] } = {}) {
+    const safeProducts = Array.isArray(products) ? products : [];
+    const hasProducts = safeProducts.length > 0;
 
     return `
-
-        <section
-            class="${WRAPPER_CLASS}"
-            aria-label="Wishlist">
-
-            ${WishlistHeader({
-
-        totalItems: products.length,
-
-    })}
-
-            ${hasProducts
-
-            ? renderProductGrid(products)
-
-            : WishlistEmpty()}
-
+        <section class="${trimClassList(WRAPPER_CLASS)}">
+            ${WishlistHeader({ totalItems: safeProducts.length })}
+            ${hasProducts ? renderProductGrid(safeProducts) : WishlistEmpty()}
         </section>
-
     `;
-
 }
